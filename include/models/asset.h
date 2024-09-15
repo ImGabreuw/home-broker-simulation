5f29856d599026ea7cd75819fe97a426fbdd964b
ImGabreuw <imgabreuw.dev@gmail.com>
2024-09-15 14:55:03 -0300

    feat: add asset entity

---
+++ include/models/asset.h
@@ -0,0 +1,18 @@
+#ifndef ASSET_H
+#define ASSET_H
+
+#include "error_codes.h"
+#include "log.h"
+
+#define MAX_CODE_LENGTH 6
+#define MAX_COMPANY_NAME_LENGTH 50
+
+typedef struct {
+    char code[MAX_CODE_LENGTH];
+    char company_name[MAX_COMPANY_NAME_LENGTH];
+    int market_volume;
+} Asset;
+
+int create_asset(Asset *asset, const char *code, const char *company_name, int market_volume);
+
+#endif // ASSET_H
---
+++ src/models/asset.c
@@ -0,0 +1,32 @@
+#include "asset.h"
+
+#include <string.h>
+
+int create_asset(Asset *asset, const char *code, const char *company_name, int market_volume) {
+    if (code == NULL || strlen(code) >= MAX_CODE_LENGTH) {
+        log_message(LOG_WARNING, "Invalid asset code\n");
+        return ERR_VALIDATION;
+    }
+
+    if (company_name == NULL || strlen(company_name) >= MAX_COMPANY_NAME_LENGTH) {
+        log_message(LOG_WARNING, "Invalid company name\n");
+        return ERR_VALIDATION;
+    }
+
+    if (market_volume < 0) {
+        log_message(LOG_WARNING, "Market volume cannot be negative\n");
+        return ERR_VALIDATION;
+    }
+
+    strncpy(asset->code, code, MAX_CODE_LENGTH - 1);
+    asset->code[MAX_CODE_LENGTH - 1] = '\0';
+
+    strncpy(asset->company_name, company_name, MAX_COMPANY_NAME_LENGTH - 1);
+    asset->company_name[MAX_COMPANY_NAME_LENGTH - 1] = '\0';
+
+    asset->market_volume = market_volume;
+
+    log_message(LOG_INFO, "Asset created: Code: %s, Company: %s, Market Volume: %d\n", asset->code, asset->company_name, asset->market_volume);
+
+    return SUCCESS;
+}
